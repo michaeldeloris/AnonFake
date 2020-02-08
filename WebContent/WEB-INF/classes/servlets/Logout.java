d0b0732740484cbb7c0b77b771ab4e7e5f660d2f
michaeldeloris <michaeldeldev@gmail.com>
2020-02-08 22:42:58 +0100

    Add /logout to logout

---
+++ WebContent/WEB-INF/classes/servlets/Logout.java
@@ -0,0 +1,26 @@
+package servlets;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class Logout
+ */
+@WebServlet("/logout")
+public class Logout extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	  request.getSession(false).invalidate();
+	  response.sendRedirect("./");
+	}
+
+}
