e17b282e6d8074d1537255b506ef3aa14b5e31ee
michaeldeloris <michaeldeldev@gmail.com>
2020-02-09 11:53:12 +0100

    Clean home servlet

--- WebContent/WEB-INF/classes/servlets/Home.java
+++ WebContent/WEB-INF/classes/servlets/Home.java
@@ -1,6 +1,5 @@
 package servlets;
 
-import java.io.File;
 import java.io.IOException;
 
 import javax.servlet.ServletException;
@@ -9,7 +8,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.Part;
 
 import util.Constants;
 import util.upload.UploadManager;
@@ -20,27 +18,16 @@ import util.upload.UploadManager;
 @MultipartConfig
 @WebServlet("")
 public class Home extends HttpServlet {
-	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Home() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
+  
+  private static final long serialVersionUID = 1L;
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+  @Override
+  protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setAttribute("page_title", Constants.TITLE_HOME);
 		request.getRequestDispatcher("/jsp/pages/home.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+  @Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	  UploadManager um = new UploadManager();
 	  um.upload(getServletContext().getRealPath(""), request);
@@ -51,5 +38,4 @@ public class Home extends HttpServlet {
       e.printStackTrace();
     }
 	}
-
 }
