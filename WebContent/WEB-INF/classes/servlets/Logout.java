a5bdad3322c104ed9043fd614023a239d70ce2c3
michaeldeloris <michaeldeldev@gmail.com>
2020-02-09 11:55:34 +0100

    Clean logout servlet

--- WebContent/WEB-INF/classes/servlets/Logout.java
+++ WebContent/WEB-INF/classes/servlets/Logout.java
@@ -6,7 +6,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Logout
@@ -15,12 +14,9 @@ import javax.servlet.http.HttpSession;
 public class Logout extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	  request.getSession(false).invalidate();
 	  response.sendRedirect("./");
 	}
-
 }
