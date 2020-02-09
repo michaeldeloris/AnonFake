12e0e7f155a35b37c93587e1bac82a228afee75e
michaeldeloris <michaeldeldev@gmail.com>
2020-02-09 11:54:45 +0100

    Clean login servlet

--- WebContent/WEB-INF/classes/servlets/Login.java
+++ WebContent/WEB-INF/classes/servlets/Login.java
@@ -19,34 +19,19 @@ import util.members.MembersManager;
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	@Override
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setAttribute("page_title", Constants.TITLE_LOGIN);
 		request.getRequestDispatcher("/jsp/pages/login.jsp").forward(request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	@Override
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	  request.setAttribute("page_title", Constants.TITLE_LOGIN);
 	  
 	  String username = request.getParameter("username");
 	  String password = request.getParameter("password");
-	  
-	  
-	  
 	  try {
       request = MembersManager.logMember(username, password, request);
     } catch (SQLException e1) {
@@ -67,5 +52,4 @@ public class Login extends HttpServlet {
 		  System.out.println(e.getMessage()); //TODO : use logger
 		}
 	}
-
 }
